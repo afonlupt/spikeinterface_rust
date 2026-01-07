5572b1dde2b0b0d563c1211f5883258087cebb71
Surame <aurelien.fonlupt@gmail.com>
2026-01-07 12:24:53 +0100

    Update dependancies

--- peak_detection/src/lib.rs
+++ peak_detection/src/lib.rs
@@ -3,7 +3,6 @@ mod rust_peak_detection_locally_exclusive;
 
 #[pymodule]
 pub fn peak_detection(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    pyo3_log::init();
     m.add_function(wrap_pyfunction!(rust_peak_detection_locally_exclusive::detect_peaks_rust_locally_exclusive_on_chunk, m)?)?;
     Ok(())
 }
